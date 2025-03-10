408750a1eb99617affa2b7779e3182789b6e2207
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2025-03-10 07:44:53 +0000

    new calculator.py added

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ create your virtual env using uv venv
 
 install the libraries
 
+Let's create a simple MCP server that exposes a calculator tool and some data:
 
+mcp install server.py -> You can install this server in Claude Desktop and interact with it right away by running:
+
+mcp dev server.py -> Alternatively, you can test it with the MCP Inspector:
 
 
--- server.py
+++ server.py
@@ -1,15 +1,7 @@
 from mcp.server.fastmcp import FastMCP
 
-#creation of an MCP SERVER
+mcp = FastMCP("My APP")
 
-mcp = FastMCP("DEMO")
+if __name__=="name":
+    mcp.run()
 
-#Adding an tool for adding two numbers
-def add(a:int,b:int) -> int:
-    return a+b
-
-
-#Adding a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name:str)->str:
-    return f"hello , {name}!"
